8823236c6fd55a45d52053c7897e91501186a666
Meridanus <graf__dracula@hotmail.com>
2022-04-30 16:05:57 +0200

    add Xp direct to Player

--- src/main/java/com/notker/xp_storage/items/Xp_Berries.java
+++ src/main/java/com/notker/xp_storage/items/Xp_Berries.java
@@ -3,6 +3,7 @@ package com.notker.xp_storage.items;
 import com.notker.xp_storage.XpStorage;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
@@ -19,12 +20,15 @@ public class Xp_Berries extends Item {
     }
 
 
-    //TODO: Why is this called sometimes 2 Times ????????
+
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         ServerWorld sw = world instanceof ServerWorld ? (ServerWorld)world : null;
-        if (sw != null) {
-            ExperienceOrbEntity.spawn(sw, user.getPos(), XpStorage.XP_PER_BERRIE);
+        PlayerEntity pe = user instanceof PlayerEntity ? (PlayerEntity) user : null;
+        if (sw != null && pe != null) {
+            //ExperienceOrbEntity.spawn(sw, user.getPos(), XpStorage.XP_PER_BERRIE);
+            //world.spawnEntity(new ExperienceOrbEntity(world, user.getX(), user.getY(), user.getZ(), XpStorage.XP_PER_BERRIE));
+            pe.addExperience(XpStorage.XP_PER_BERRIE);
         }
 
        return super.finishUsing(stack, world, user);
