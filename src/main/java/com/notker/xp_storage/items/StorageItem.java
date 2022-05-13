ac76852e7a3f9205527077ef6563fe69c03b5708
Meridanus <graf__dracula@hotmail.com>
2022-05-13 23:27:29 +0200

    add shift tooltip

--- src/main/java/com/notker/xp_storage/items/StorageItem.java
+++ src/main/java/com/notker/xp_storage/items/StorageItem.java
@@ -1,7 +1,10 @@
 package com.notker.xp_storage.items;
 
+import com.notker.xp_storage.XpStorage;
 import com.notker.xp_storage.regestry.ModItems;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -20,19 +23,24 @@ public class StorageItem extends Item {
     @Override
     public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
 
-        if (this.equals(ModItems.INSPECTOR)) {
-            tooltip.add( new TranslatableText("item.tooltip.inspector").formatted(Formatting.AQUA));
+        tooltip.add(new TranslatableText("item.xps.moreinfo.tooltip"));
+        if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), XpStorage.shiftKey)) {
+
+
+            tooltip.remove(new TranslatableText("item.xps.moreinfo.tooltip"));
+            if (this.equals(ModItems.INSPECTOR)) {
+                tooltip.add(new TranslatableText("item.tooltip.inspector").formatted(Formatting.AQUA));
+            }
+
+            if (this.equals(ModItems.LOCK)) {
+                tooltip.add(new TranslatableText("item.tooltip.lock").formatted(Formatting.AQUA));
+            }
+
+            if (this.equals(ModItems.KEY)) {
+                tooltip.add(new TranslatableText("item.tooltip.key").formatted(Formatting.AQUA));
+            }
+
         }
-
-        if (this.equals(ModItems.LOCK)) {
-            tooltip.add( new TranslatableText("item.tooltip.lock").formatted(Formatting.AQUA));
-        }
-
-        if (this.equals(ModItems.KEY)) {
-            tooltip.add( new TranslatableText("item.tooltip.key").formatted(Formatting.AQUA));
-        }
-
-
     }
 }
 
